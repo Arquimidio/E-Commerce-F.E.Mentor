ffe05fd872c258143c4a39051a22e7c2c1d828f0
Arquimidio <gabrielstoppadossantos@gmail.com>
2021-10-12 16:42:04 -0300

    Added mobile functionalities to cart

--- script/app.js
+++ script/app.js
@@ -1,21 +1,45 @@
 const modals = (function(){
     const mobileModal = document.getElementById('mobile-modal')
-    const openMobileMenu = document.getElementById('hamburguer-menu')
-    const closeMobileMenu = document.getElementById('mobile-menu-close-button')
+    const openMobileMenuBtn = document.getElementById('hamburguer-menu')
+    const closeMobileMenuBtn = document.getElementById('mobile-menu-close-button')
+    const cartModal = document.getElementById('mobile-cart')
+    const openCartBtn = document.getElementById('nav-cart')
+
+    function closeMobileMenu(){
+        mobileModal.classList.add('hide')
+    }
+
+    function openMobileMenu(){
+        mobileModal.classList.remove('hide')
+    }
+
+    function openCart(){
+        cartModal.classList.remove('hide')
+    }
+
+    function verifyClick(e){
+        if(!cartModal.contains(e.target) && 
+           !document.querySelector('nav').contains(e.target)){
+            cartModal.classList.add('hide')
+        }
+    }
+    
     return{
         mobileModal,
+        openMobileMenuBtn,
+        closeMobileMenuBtn,
+        cartModal,
+        openCartBtn,
+        closeMobileMenu,
         openMobileMenu,
-        closeMobileMenu
+        openCart,
+        verifyClick
     }
 })()
 
-modals.closeMobileMenu.addEventListener('touchstart', closeMobileMenu)
-modals.openMobileMenu.addEventListener('touchstart', openMobileMenu)
+modals.closeMobileMenuBtn.addEventListener('touchstart', modals.closeMobileMenu)
+modals.openMobileMenuBtn.addEventListener('touchstart', modals.openMobileMenu)
+modals.openCartBtn.addEventListener('touchstart', modals.openCart)
+document.addEventListener('click', modals.verifyClick)
 
-function closeMobileMenu(){
-    modals.mobileModal.style.display = 'none'
-}
 
-function openMobileMenu(){
-    modals.mobileModal.style.display = 'block'
-}
