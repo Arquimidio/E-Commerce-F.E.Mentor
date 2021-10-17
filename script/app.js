295f0e66016962b115c6322cddd9eaf60c36fe4b
Arquimidio <gabrielstoppadossantos@gmail.com>
2021-10-17 11:10:56 -0300

    Created lightbox

--- script/app.js
+++ script/app.js
@@ -4,6 +4,8 @@ const modals = (function(){
     const closeMobileMenuBtn = document.getElementById('mobile-menu-close-button')
     const cartModal = document.getElementById('mobile-cart')
     const openCartBtn = document.getElementById('nav-cart')
+    const thumbnails = [...document.getElementById('thumbs-container').children]
+    const lightbox = document.getElementById('lightbox-area')
 
     function closeMobileMenu(){
         mobileModal.classList.add('hide')
@@ -17,6 +19,10 @@ const modals = (function(){
         cartModal.classList.remove('hide')
     }
 
+    function openLightBox(){
+        lightbox.style.display = 'flex'
+    }
+
     function verifyClick(e){
         if(!cartModal.contains(e.target) && 
            !document.querySelector('nav').contains(e.target) 
@@ -24,24 +30,15 @@ const modals = (function(){
             cartModal.classList.add('hide')
         }
     }
-    
-    return{
-        mobileModal,
-        openMobileMenuBtn,
-        closeMobileMenuBtn,
-        cartModal,
-        openCartBtn,
-        closeMobileMenu,
-        openMobileMenu,
-        openCart,
-        verifyClick
-    }
+
+    closeMobileMenuBtn.addEventListener('click', closeMobileMenu)
+    openMobileMenuBtn.addEventListener('click', openMobileMenu)
+    openCartBtn.addEventListener('click', openCart)
+    thumbnails.map(elt => elt.addEventListener('click', openLightBox))
+    document.addEventListener('click', verifyClick)  
+
 })()
 
-modals.closeMobileMenuBtn.addEventListener('click', modals.closeMobileMenu)
-modals.openMobileMenuBtn.addEventListener('click', modals.openMobileMenu)
-modals.openCartBtn.addEventListener('click', modals.openCart)
-document.addEventListener('click', modals.verifyClick)
 
 /*******************IMAGE CHANGER*********************** */
 const imgFunctionalities = (function(){
@@ -157,7 +154,7 @@ const cart = (function(){
     }
 
     function removeFromCart(){
-        let parent = this.closest('.cart-product-wrapper')
+        const parent = this.closest('.cart-product-wrapper')
         cartProducts.removeChild(parent)
         if(cartProducts.childNodes.length <= 3){
             document.getElementById('empty-cart').classList.remove('hide')
@@ -182,6 +179,7 @@ const cart = (function(){
         let name = createElt('p', 'product-cart-name')
         let price = createElt('p', 'product-cart-price')
         let remove = createElt('div', 'delete-from-cart')
+        let eltArr = [img, description, name, price, remove]
         remove.addEventListener('click', removeFromCart)
         strong.textContent = calculatePrice(getProductAmount(), getUnityPrice())
         name.textContent = getProductName()
@@ -189,9 +187,9 @@ const cart = (function(){
         price.appendChild(strong)
         img.src = 'images/image-product-1-thumbnail.jpg'
         remove.innerHTML = '<svg width="14" height="16" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><path d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4.5 6.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Z" id="a"/></defs><use fill="#C3CAD9" fill-rule="nonzero" xlink:href="#a"/></svg>'
-        let eltArr = [img, description, remove]
         description.appendChild(name)
         description.appendChild(price)
+        
         eltArr.forEach(elt => wrapper.appendChild(elt))
         cartProducts.appendChild(wrapper)
         showCheckoutBtn(true)
